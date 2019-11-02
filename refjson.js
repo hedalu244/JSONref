c938a6eb6c6be2433b027bc19108f80305b73965
Yuki-jukjis <taka0618.m@gmail.com>
2019-11-02 15:52:14 +0900

    パーサコンビネータ

--- refjson.js
+++ refjson.js
@@ -25,3 +25,78 @@ function stringify(value, dic=[], path="self") {
   }
 }
 
+function parse(text) {
+  function string(token) {
+    return function(text) {
+      if(text.startsWith(token))
+        return {success:true, result:token, rest:text.substring(token.length)};
+      return {success:false, result:null, rest:text};
+    };
+  }
+  function regexp(pattern) {
+    pattern = new RegExp(pattern.source.startsWith("^") ? pattern.source : "^" + pattern.source)
+    return function(text) {
+      var result = pattern.exec(text);
+      if(result)
+        return {success:true, result:result[0], rest:text.substring(result[0].length)};
+      return {success:false, result:null, rest:text};
+    };
+  }
+  function choice(/* parsers... */) {
+    var parsers = arguments;
+    return function(text) {
+      for (var i = 0; i < parsers.length; i++) {
+        var parsed = parsers[i](text);
+        if (parsed.success)
+          return parsed;
+        return {success:false, result:null, rest:text};
+      };
+    }
+  }
+  //0個以上
+  function many(parser) {
+    return function(text) {
+      var result = [];
+      while(true) {
+        var parsed = parser(text);
+        if (parsed.success) {
+          result.push(parsed.result);
+          text = parsed.rest;
+        } else {
+          return {success:true, result:result, rest:text};
+        }
+      }
+    }
+  }
+  function seq(/* parsers... */) {
+    var parsers = arguments;
+    return function(text) {
+      var result = [];
+      for (var i = 0; i < parsers.length; i++) {
+        var parsed = parsers[i](text);
+        if (parsed.success) {
+          result.push(parsed.result);
+          text = parsed.rest;
+        } else {
+          return {success:false, result:null, rest:text};
+        }
+      }
+      return {success:true, result:result, rest:text};
+    };
+  }
+  function lazy(callback) {
+    return function(text) {
+      return callback()(text);
+    };
+  }
+  function map(parser, fn) {
+    return function(text) {
+      var parsed = parser(text);
+      if (parsed.success) {
+        return {success:true, result:fn(parsed.result), rest:text};
+      } else {
+        return {success:false, result:null, rest:text};
+      }
+    }
+  }
+}
